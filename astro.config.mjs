2aac44ca3ed6a53523b4f1f5b8fa9a9f56261ed4
Keisuke Watanabe <watanabe@cytokine.jp>
2022-11-10 09:52:01 +0900

    astro-test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image()],
+  trailingSlash: 'ignore',
+  site: 'https://www.my-site.dev'
+});
